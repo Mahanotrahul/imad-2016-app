1bcace0ea574e1a626c1d2ba4871d4b00966a86c
Rahul Mahanot <rahulmahanott08@gmail.com>
2016-10-26 01:01:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'mahanotrahul',
+    database: 'mahanotrahul',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -15,8 +23,19 @@ app.get('/counter',function(req,res){
     
 });
 
-
-app.get('test-db',function(req,res){
+var pool = new Pool(config)
+app.get('/test-db',function(req,res)
+{
+    pool.query('SELECT * FROM test', function(err,result)
+    {
+        if(err)
+        {
+        res.status(500).send(err.toString());
+        }
+        else{
+        res.status(JSON.stringify(result));
+        }
+    })
     
 })
 app.get('/ui/style.css', function (req, res) {
