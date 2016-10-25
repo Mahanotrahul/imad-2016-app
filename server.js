721b991527ec6308e96edc110dc1da84b00cf341
Rahul Mahanot <rahulmahanott08@gmail.com>
2016-10-26 00:45:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/counter',function(req,res){
     
 });
 
+
+app.get('test-db',function(req,res){
+    
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
